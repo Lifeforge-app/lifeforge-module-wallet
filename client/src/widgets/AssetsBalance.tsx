f9395c07be5595568349e6a62abdc8381c996761
melvinchia3636 <melvinchia623600@gmail.com>
2025-11-03 11:29:47 +0800

    fix: fix empty state localization of widget

--- client/src/widgets/AssetsBalance.tsx
+++ client/src/widgets/AssetsBalance.tsx
@@ -1,5 +1,10 @@
 import { Icon } from '@iconify/react'
-import { Button, DashboardItem, WithQuery } from 'lifeforge-ui'
+import {
+  Button,
+  DashboardItem,
+  EmptyStateScreen,
+  WithQuery
+} from 'lifeforge-ui'
 import { useState } from 'react'
 import { Link } from 'shared'
 import type { WidgetConfig } from 'shared'
@@ -29,52 +34,62 @@ export default function AssetsBalance() {
       title="Assets Balance"
     >
       <WithQuery query={assetsQuery}>
-        {assets => (
-          <ul className="grid h-full grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-2 overflow-y-auto">
-            {assets.map(asset => (
-              <Link
-                key={asset.id}
-                className="flex-between bg-bg-100 component-bg-lighter-with-hover flex h-full gap-3 rounded-lg p-2 pl-4 pr-0 shadow-[4px_4px_10px_rgba(0,0,0,0.1)] transition-all"
-                to={'/wallet/assets'}
-              >
-                <div className="flex w-full min-w-0 items-center gap-3">
-                  <div className="bg-bg-200 dark:bg-bg-700 rounded-md p-2">
-                    <Icon
-                      className="text-bg-500 dark:text-bg-100 size-6"
-                      icon={asset.icon}
-                    />
-                  </div>
-                  <div className="flex w-full min-w-0 flex-col">
-                    <div className="w-full min-w-0 truncate font-semibold">
-                      {asset.name}
+        {assets =>
+          assets.length !== 0 ? (
+            <EmptyStateScreen
+              smaller
+              icon="tabler:wallet-off"
+              name="assets"
+              namespace="apps.wallet"
+              tKey="widgets.assetsBalance"
+            />
+          ) : (
+            <ul className="grid h-full grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-2 overflow-y-auto">
+              {assets.map(asset => (
+                <Link
+                  key={asset.id}
+                  className="flex-between bg-bg-100 component-bg-lighter-with-hover flex h-full gap-3 rounded-lg p-2 pl-4 pr-0 shadow-[4px_4px_10px_rgba(0,0,0,0.1)] transition-all"
+                  to={'/wallet/assets'}
+                >
+                  <div className="flex w-full min-w-0 items-center gap-3">
+                    <div className="bg-bg-200 dark:bg-bg-700 rounded-md p-2">
+                      <Icon
+                        className="text-bg-500 dark:text-bg-100 size-6"
+                        icon={asset.icon}
+                      />
                     </div>
-                    <div className="text-bg-500 flex items-center gap-1 text-sm">
-                      RM{' '}
-                      {showBalance ? (
-                        numberToCurrency(asset.current_balance)
-                      ) : (
-                        <span className="flex items-center">
-                          {Array(4)
-                            .fill(0)
-                            .map((_, i) => (
-                              <Icon
-                                key={i}
-                                className="size-3"
-                                icon="uil:asterisk"
-                              />
-                            ))}
-                        </span>
-                      )}
+                    <div className="flex w-full min-w-0 flex-col">
+                      <div className="w-full min-w-0 truncate font-semibold">
+                        {asset.name}
+                      </div>
+                      <div className="text-bg-500 flex items-center gap-1 text-sm">
+                        RM{' '}
+                        {showBalance ? (
+                          numberToCurrency(asset.current_balance)
+                        ) : (
+                          <span className="flex items-center">
+                            {Array(4)
+                              .fill(0)
+                              .map((_, i) => (
+                                <Icon
+                                  key={i}
+                                  className="size-3"
+                                  icon="uil:asterisk"
+                                />
+                              ))}
+                          </span>
+                        )}
+                      </div>
                     </div>
                   </div>
-                </div>
-                <button className="text-bg-300 dark:text-bg-700 rounded-lg p-4 transition-all">
-                  <Icon className="text-xl" icon="tabler:chevron-right" />
-                </button>
-              </Link>
-            ))}
-          </ul>
-        )}
+                  <button className="text-bg-300 dark:text-bg-700 rounded-lg p-4 transition-all">
+                    <Icon className="text-xl" icon="tabler:chevron-right" />
+                  </button>
+                </Link>
+              ))}
+            </ul>
+          )
+        }
       </WithQuery>
     </DashboardItem>
   )
